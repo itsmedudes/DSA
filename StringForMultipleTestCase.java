3c5e7ea5cc0cb82078311ee5d8aa7730557abddc
Dilip Kumar <31159055+itsmedudes@users.noreply.github.com>
2022-11-18 11:38:05 +0530

    Update StringForMultipleTestCase.java

--- StringForMultipleTestCase.java
+++ StringForMultipleTestCase.java
@@ -1,7 +1,7 @@
 package com.masai.Input;
 
 import java.util.*;
-public class StringForMultipleTestCase {
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
