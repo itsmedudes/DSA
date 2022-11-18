2ef21fd0d08f4c35073bcfef7b2f5c587d311b37
Dilip Kumar <31159055+itsmedudes@users.noreply.github.com>
2022-11-18 11:15:18 +0530

    Update SingleMatrix.java

--- SingleMatrix.java
+++ SingleMatrix.java
@@ -3,7 +3,7 @@ package com.masai.Input;
 import java.util.*;
 
 
-public class SingleMatrix {
+public class Main {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
